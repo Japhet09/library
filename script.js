423e5edbdaf43eb9de92b074a6c8cfa35ffab9f0
Japhet09 <japhetalfred19@gmail.com>
2021-12-20 22:09:00 +0100

    Refactored the code and added a function for user to input books through prompt

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-function Book (title, author,pages, isRead=true){ //constructor function
+function Book(title, author, pages, isRead) {//constructor function
     this.title = title
     this.author = author
     this.pages = pages
-    isRead?this.isRead = 'I have read it':this.isRead = 'not read yet'
 
-
+    if (isRead==='Yes'){
+        this.isRead = 'I have read it'
+    } else {
+        this.isRead = ' not read yet'
     }
+   //Same as above if statement
+     //isRead==='Yes'?this.isRead = 'I have read it':this.isRead = 'not read yet'
+}
 
-Book.prototype.info = function(){ // info method inheritable by all instances of Book constructor
+Book.prototype.info = function () { // info method inheritable by all instances of Book Constructor
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`
-    };
-
-let myLibrary = [] // array to store all the book objects
-function addBookToLibrary(title,author,pages,isRead){
-    let book1 = new Book (title, author,pages,isRead)
-    myLibrary.push(book1)
-    return myLibrary
- 
 }
-addBookToLibrary('Atomic Habits','James Clear',184, false)
-addBookToLibrary('The Alchemist', 'Paulo Coelho', 146, true)
-addBookToLibrary('Black Beauty', 'Anna sewel', 55,true)
-addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 231, false)
 
-function displayBooks(library){
-    for(i=0;i<=library.length;i++){
-        console.table(library[i])
-    }
+let bornACrime = new Book('Born A Crime', 'Trevor Noah', 165, 'Yes')
+
+
+let myLibrary = []
+
+function addBookToLibrary() {
+    let title = prompt('Whats the title of the book?')
+    let author = prompt('Who is the author?')
+    let pages = parseInt(prompt('How many pages?'))
+    let isRead = prompt('Have you read the book? Yes or No')
+    let book = new Book(title, author, pages, isRead)
+    myLibrary.push(book)
+
 }
+for(let i=0; i<3; i++){
+addBookToLibrary()}
+
+
